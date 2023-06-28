5538dac7dea4ecfb4bb25a93972f4a32f46846e3
Je-Carlos <cpjeanberg@gmail.com>
2023-06-28 20:26:56 -0300

    alterando index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
-import "./reset.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
