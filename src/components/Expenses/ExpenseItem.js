587746cff7227986389475adad4e526dd2071b69
Je-Carlos <cpjeanberg@gmail.com>
2023-06-30 12:04:36 -0300

    adicionando uppercase na primeira letra do titulo

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,11 +3,14 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const upperCaseTitle =
+    props.title.charAt(0).toUpperCase() + props.title.slice(1);
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{upperCaseTitle}</h2>
       </div>
       <div className="expense-item__price">R$ {props.amount}</div>
     </Card>
