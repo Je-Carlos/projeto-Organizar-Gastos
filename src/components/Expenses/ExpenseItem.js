0a1612c7f29c63ea385611597db593865bc48ef0
Je-Carlos <cpjeanberg@gmail.com>
2023-06-30 12:14:50 -0300

    convertendo p Number e add toFixed(2) aos valores passados

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,16 +3,15 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const upperCaseTitle =
-    props.title.charAt(0).toUpperCase() + props.title.slice(1);
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{upperCaseTitle}</h2>
+        <h2>{props.title.charAt(0).toUpperCase() + props.title.slice(1)}</h2>
       </div>
-      <div className="expense-item__price">R$ {props.amount}</div>
+      <div className="expense-item__price">
+        R$ {Number(props.amount).toFixed(2)}
+      </div>
     </Card>
   );
 };
